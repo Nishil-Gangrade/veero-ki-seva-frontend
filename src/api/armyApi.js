7baac1e97e4b76953e93661fed9f2c9d1a90978e
Nishil Gangrade <nishilgangrade2003@gmail.com>
2025-07-21 00:01:36 +0530

    bugs correction

--- src/api/armyApi.js
+++ src/api/armyApi.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log("API_BASE_URL: ", API_BASE_URL);
+console.log("API_BASE_URL is : ", API_BASE_URL);
 
 // Submit Army Event
 export const submitArmyEvent = async (eventData) => {
